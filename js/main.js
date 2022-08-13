19b8422161eb68234df48207da2acb6aafdfaa3d
Joxa-UZ <javokhirrustamovuz@gmail.com>
2022-08-13 10:08:00 +0500

    hero

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var elModalOpenButton = document.querySelector(".site-header__hamburger");
+var elModalOpenlink = document.querySelector(".site-header__finish");
+
+elModalOpenButton.addEventListener("click", function (){
+    elModalOpenlink.classList.toggle("site-header__finish--active");
+});
